a12dac99d3eb1a96b6a681e8f7a6d59625daca1d
Keng <kengzwl@gmail.com>
2015-11-30 05:44:56 -0500

    comment out main in agent

--- aigent/soccerpy/agent.py
+++ aigent/soccerpy/agent.py
@@ -322,61 +322,61 @@ class Agent:
                 return
 
 
-if __name__ == "__main__":
-    import sys
-    import multiprocessing as mp
+# if __name__ == "__main__":
+#     import sys
+#     import multiprocessing as mp
 
-    # enforce corrent number of arguments, print help otherwise
-    if len(sys.argv) < 3:
-        print "args: ./agent.py <team_name> <num_players>"
-        sys.exit()
+#     # enforce corrent number of arguments, print help otherwise
+#     if len(sys.argv) < 3:
+#         print "args: ./agent.py <team_name> <num_players>"
+#         sys.exit()
 
-    def spawn_agent(team_name):
-        """
-        Used to run an agent in a seperate physical process.
-        """
+#     def spawn_agent(team_name):
+#         """
+#         Used to run an agent in a seperate physical process.
+#         """
 
-        a = Agent()
-        a.connect("localhost", 6000, team_name)
-        a.play()
+#         a = Agent()
+#         a.connect("localhost", 6000, team_name)
+#         a.play()
 
-        # we wait until we're killed
-        while 1:
-            # we sleep for a good while since we can only exit if terminated.
-            time.sleep(1)
+#         # we wait until we're killed
+#         while 1:
+#             # we sleep for a good while since we can only exit if terminated.
+#             time.sleep(1)
 
-    # spawn all agents as seperate processes for maximum processing efficiency
-    agentthreads = []
-    for agent in xrange(min(11, int(sys.argv[2]))):
-        print "  Spawning agent %d..." % agent
+#     # spawn all agents as seperate processes for maximum processing efficiency
+#     agentthreads = []
+#     for agent in xrange(min(11, int(sys.argv[2]))):
+#         print "  Spawning agent %d..." % agent
 
-        at = mp.Process(target=spawn_agent, args=(sys.argv[1],))
-        at.daemon = True
-        at.start()
+#         at = mp.Process(target=spawn_agent, args=(sys.argv[1],))
+#         at.daemon = True
+#         at.start()
 
-        agentthreads.append(at)
+#         agentthreads.append(at)
 
-    print "Spawned %d agents." % len(agentthreads)
-    print
-    print "Playing soccer..."
+#     print "Spawned %d agents." % len(agentthreads)
+#     print
+#     print "Playing soccer..."
 
-    # wait until killed to terminate agent processes
-    try:
-        while 1:
-            time.sleep(0.05)
-    except KeyboardInterrupt:
-        print
-        print "Killing agent threads..."
+#     # wait until killed to terminate agent processes
+#     try:
+#         while 1:
+#             time.sleep(0.05)
+#     except KeyboardInterrupt:
+#         print
+#         print "Killing agent threads..."
 
-        # terminate all agent processes
-        count = 0
-        for at in agentthreads:
-            print "  Terminating agent %d..." % count
-            at.terminate()
-            count += 1
-        print "Killed %d agent threads." % (count - 1)
+#         # terminate all agent processes
+#         count = 0
+#         for at in agentthreads:
+#             print "  Terminating agent %d..." % count
+#             at.terminate()
+#             count += 1
+#         print "Killed %d agent threads." % (count - 1)
 
-        print
-        print "Exiting."
-        sys.exit()
+#         print
+#         print "Exiting."
+#         sys.exit()
 
