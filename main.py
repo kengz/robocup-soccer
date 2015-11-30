f56a2d32a6627da35106af62fbcffda5c50613f2
Keng <kengzwl@gmail.com>
2015-11-30 02:11:11 -0500

    corrected default agent to a1

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ if __name__ == "__main__":
     # return type of agent: midfield, striker etc.
     def agent_type(position):
     	return {
-    		'0': A0,
+    		# '0': A0,
     		'1': A1,
-    	}.get(position, A0)
+    	}.get(position, A1)
 
     # spawn an agent of team_name, with position
     def spawn_agent(team_name, position):
