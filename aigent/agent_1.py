ae2d14d4e6a88238a5768bce28646ccc6cddb669
Keng <kengzwl@gmail.com>
2015-11-30 05:06:24 -0500

    added stupid backup

--- aigent/agent_1.py
+++ aigent/agent_1.py
@@ -306,24 +306,70 @@ class Agent(baseAgent):
 
 
     def decisionLoop(self):
-        self.find_ball()
-        # if should shoot, full power
-        if self.shall_shoot():
-            return self.shoot()
-        # else shd pass to closest teammate
-        elif self.shall_pass():
-            return self.passes()
-        # else shd dribble
-        elif self.shall_dribble():
-            return self.dribble()
-        elif self.shall_move_to_ball():
-            return self.move_to_ball()
-        elif self.shall_move_to_defend():
-            return self.move_to_defend()
-        elif self.shall_move_to_enemy_goalpos():
-            return self.move_to_enemy_goalpos()
-        else:
-            return self.lookaround()
+        try:
+            self.find_ball()
+            # if should shoot, full power
+            if self.shall_shoot():
+                return self.shoot()
+            # else shd pass to closest teammate
+            elif self.shall_pass():
+                return self.passes()
+            # else shd dribble
+            elif self.shall_dribble():
+                return self.dribble()
+            elif self.shall_move_to_ball():
+                return self.move_to_ball()
+            elif self.shall_move_to_defend():
+                return self.move_to_defend()
+            elif self.shall_move_to_enemy_goalpos():
+                return self.move_to_enemy_goalpos()
+            else:
+                return self.lookaround()
+        except:
+              # kick off!
+            if self.wm.is_before_kick_off():
+                # player 9 takes the kick off
+                if self.wm.uniform_number == 9:
+                    if self.wm.is_ball_kickable():
+                        # kick with 100% extra effort at enemy goal
+                        self.wm.kick_to(goal_pos, 1.0)
+                    else:
+                        # move towards ball
+                        if self.wm.ball is not None:
+                            if (self.wm.ball.direction is not None and
+                                    -7 <= self.wm.ball.direction <= 7):
+                                self.wm.ah.dash(50)
+                            else:
+                                self.wm.turn_body_to_point((0, 0))
+    
+                    # turn to ball if we can see it, else face the enemy    goal
+                    if self.wm.ball is not None:
+                        self.wm.turn_neck_to_object(self.wm.ball)
+    
+                    return
+    
+            # attack!
+            else:
+                # find the ball
+                if self.wm.ball is None or self.wm.ball.direction is None:
+                    self.wm.ah.turn(30)
+    
+                    return
+    
+                # kick it at the enemy goal
+                if self.wm.is_ball_kickable():
+                    self.wm.kick_to(goal_pos, 1.0)
+                    return
+                else:
+                    # move towards ball
+                    if -7 <= self.wm.ball.direction <= 7:
+                        self.wm.ah.dash(65)
+                    else:
+                        # face ball
+                        self.wm.ah.turn(self.wm.ball.direction / 2)
+    
+                    return
+
         
 
 
