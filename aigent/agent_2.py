baa86298227390657f3cb7c5f388d4696af2b9d8
Keng <kengzwl@gmail.com>
2015-11-30 06:18:53 -0500

    bypass other acts for def

--- aigent/agent_2.py
+++ aigent/agent_2.py
@@ -314,7 +314,8 @@ class Agent(baseAgent):
         try:
             if self.wm.get_distance_to_point(self.own_goal_pos) > 40:
                 self.wm.turn_body_to_point(self.own_goal_pos)
-                self.wm.ah.dash(50)
+                self.wm.ah.dash(70)
+            return
             self.find_ball()
             # if should shoot, full power
             if self.shall_shoot():
