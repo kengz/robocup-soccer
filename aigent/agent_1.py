0465b3c7307c53e3a0b2c1b9c1a61356b215136d
Keng <kengzwl@gmail.com>
2015-11-30 05:19:04 -0500

    look as backp

--- aigent/agent_1.py
+++ aigent/agent_1.py
@@ -181,25 +181,71 @@ class Agent(baseAgent):
         # return
 
 
-        # find the ball
-        if self.wm.ball is None or self.wm.ball.direction is None:
-            self.wm.ah.turn(random.randrange(-30,30))
+        # # find the ball
+        # if self.wm.ball is None or self.wm.ball.direction is None:
+        #     self.wm.ah.turn(random.randrange(-30,30))
 
-            return
+        #     return
 
-        # kick it at the enemy goal
-        if self.wm.is_ball_kickable():
-            self.wm.kick_to(self.enemy_goal_pos, 1.0)
-            return
+        # # kick it at the enemy goal
+        # if self.wm.is_ball_kickable():
+        #     self.wm.kick_to(self.enemy_goal_pos, 1.0)
+        #     return
+        # else:
+        #     # move towards ball
+        #     if -7 <= self.wm.ball.direction <= 7:
+        #         self.wm.ah.dash(65)
+        #     else:
+        #         # face ball
+        #         self.wm.ah.turn(self.wm.ball.direction / 2)
+
+        #     return
+
+        # kick off!
+        if self.wm.is_before_kick_off():
+            # player 9 takes the kick off
+            if self.wm.uniform_number == 9:
+                if self.wm.is_ball_kickable():
+                    # kick with 100% extra effort at enemy goal
+                    self.wm.kick_to(self.enemy_goal_pos, 1.0)
+                else:
+                    # move towards ball
+                    if self.wm.ball is not None:
+                        if (self.wm.ball.direction is not None and
+                                -7 <= self.wm.ball.direction <= 7):
+                            self.wm.ah.dash(50)
+                        else:
+                            self.wm.turn_body_to_point((0, 0))
+
+                # turn to ball if we can see it, else face the enemy goal
+                if self.wm.ball is not None:
+                    self.wm.turn_neck_to_object(self.wm.ball)
+
+                return
+
+        # attack!
         else:
-            # move towards ball
-            if -7 <= self.wm.ball.direction <= 7:
-                self.wm.ah.dash(65)
-            else:
-                # face ball
-                self.wm.ah.turn(self.wm.ball.direction / 2)
+            # find the ball
+            if self.wm.ball is None or self.wm.ball.direction is None:
+                self.wm.ah.turn(30)
 
-            return
+                return
+
+            # kick it at the enemy goal
+            if self.wm.is_ball_kickable():
+                self.wm.kick_to(self.enemy_goal_pos, 1.0)
+                return
+            else:
+                # move towards ball
+                if -7 <= self.wm.ball.direction <= 7:
+                    self.wm.ah.dash(65)
+                else:
+                    # face ball
+                    self.wm.ah.turn(self.wm.ball.direction / 2)
+
+                return
+
+
 
     # condition for shooting to the goal
     def shall_shoot(self):
@@ -238,8 +284,8 @@ class Agent(baseAgent):
     # condition for dribbling, if can't shoot or pass
     def shall_dribble(self):
         # find the ball
-        if self.wm.ball is None or self.wm.ball.direction is None:
-            self.wm.ah.turn(30)
+        # if self.wm.ball is None or self.wm.ball.direction is None:
+            # self.wm.ah.turn(30)
         return self.wm.is_ball_kickable()
 
     # dribble: turn body, kick, then run towards ball
